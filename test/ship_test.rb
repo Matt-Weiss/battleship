03f83b2eafda56422a9b10ed51e9ae43513447cf
Smitha Hosmani <smitha_hosmani@yahoo.com>
2019-01-29 11:58:41 -0700

    add attributes test

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -9,4 +9,9 @@ class ShipTest < Minitest::Test
     assert_instance_of Ship, cruiser
   end
 
+  def test_ship_attributes
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal "Cruiser", cruiser.name
+    assert_equal 3, cruiser.length
+  end
 end
