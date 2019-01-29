f4389d0bb38d161d77f38889da3dd9ecdd2e28e2
Smitha Hosmani <smitha_hosmani@yahoo.com>
2019-01-29 12:43:30 -0700

    complete ship methods

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,9 +1,18 @@
 class Ship
 
-  attr_reader :name, :length
+  attr_reader :name, :length, :health
   def initialize(type_of_ship, length_of_ship)
     @name = type_of_ship
     @length = length_of_ship
+    @health = length_of_ship
+  end
+
+  def hit
+    @health -= 1
+  end
+
+  def sunk?
+    @health == 0
   end
 
 end
