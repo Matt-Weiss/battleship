6420e645499a217ac77939711e122b67ebf3d0fc
Smitha Hosmani <smitha_hosmani@yahoo.com>
2019-02-04 17:11:25 -0700

    cleaned up white space, added interpolation for cells.

--- lib/messaging.rb
+++ lib/messaging.rb
@@ -1,92 +1,122 @@
 class Messaging
 
   def welcome_message
-    p "--------- Welcome to Battleship ---------"
-    p "-------------Enter p to play-------------"
-    p ""
-    p "I bet you have a sinking feeling right now..."
-    p "-------------Enter q to quit-------------"
+    puts "--------- Welcome to Battleship ---------"
+    puts "-------------Enter p to play-------------"
+    puts "-------------Enter q to quit-------------"
+    puts ""
   end
 
+  def computer_ships_placed
+    puts ""
+    puts "I have placed my ships. Now it's your turn."
+    puts "I bet you have a sinking feeling right now..."
+    puts ""
+  end
+
+
   def player_ship_placement_instruction
-    p "We each get two ships, one cruiser and one submarine."
-    p "The cruiser occupies three cells."
-    p "The submarine occupies two cells."
-    p "------------------------------------------------------"
-    p "Enter your coordinates in the following format: "
-    p "(example: C2 C3 C4 for cruiser or A1 B1 for submarine)"
-    p "-" * 25
+    puts "We each get two ships, one cruiser and one submarine."
+    puts "The cruiser occupies three cells."
+    puts "The submarine occupies two cells."
+    puts "------------------------------------------------------"
+    puts "Enter your coordinates in the following format: "
+    puts "(example: C2 C3 C4 for cruiser or A1 B1 for submarine)"
+    puts "-" * 25
   end
 
   def player_valid_coordinates_cruiser
-    p "Enter coordinates for your cruiser: "
+    puts "Enter coordinates for your cruiser: "
   end
 
   def player_valid_coordinates_submarine
-    p "Enter coordinates for your submarine: "
+    puts "Enter coordinates for your submarine: "
   end
 
   def player_invalid_coordinates
-    p "I have a sinking feeling these are invalid coordinates, try again."
+    puts "I have a sinking feeling these are invalid coordinates, try again."
   end
 
   def ready_to_play
-    p "-" * 25
-    p "******Prepare for battle!******"
-    p "-" * 25
-    p "Before you fire at me, you should know:"
-    p "'M' for missed shots."
-    p "'H' for hitting a cell with a ship."
-    p "'X' for a sunken ship."
-    p "'.' for empty cell."
+    puts "******Prepare for battle!******"
+    puts "Before you fire at me, you should know:"
+    puts "'M' for missed shots."
+    puts "'H' for hitting a cell with a ship."
+    puts "'X' for a sunken ship."
+    puts "'.' for empty cell."
+  end
+
+  def computer_board_header
+    puts ""
+    puts ("=" * 15) + "COMPUTER BOARD" + ("=" * 15)
+  end
+
+  def player_board_header
+    puts ("=" * 15) + "YOUR BOARD" + ("=" * 15)
   end
 
   def player_fires_upon
-    p "See if you can sink me. Enter the coordinate for your shot:"
+    print "See if you can sink me. Enter the coordinate for your shot: "
+    puts ""
   end
 
   def player_invalid_coordinates_to_fire_upon
-    p "If you want to sink me, pick a better coordinate."
+    puts "If you want to sink me, pick a better coordinate."
   end
 
-  def player_shot_missed
-    p "Your shot on (cell) was a miss."
+  def player_shot_missed(cell)
+    puts ""
+    puts "Your shot on #{cell} was a miss."
   end
 
-  def player_shot_hits
-    p "Your shot on (cell) hit me!"
+  def player_shot_hits(cell)
+    puts ""
+    puts "Your shot on #{cell} hit me!"
   end
 
-  def player_sinks_ship
-    p "Blimey! Your shot on (cell) sunk me ship!"
+  def player_sinks_ship(cell)
+    puts ""
+    puts "Blimey! Your shot on #{cell} sunk me ship!"
   end
 
-  def computer_shot_missed
-    p "My shot on (cell) was a miss."
+  def computer_shot_missed(cell)
+    puts ""
+    puts "My shot on #{cell} was a miss."
   end
 
-  def computer_shot_hits
-    p "My shot on (cell) was hit."
+  def computer_shot_hits(cell)
+    puts ""
+    puts "My shot on #{cell} was hit."
   end
 
-  def computer_shot_sinks
-    p "Aye, my shot on (cell) sunk your ship!"
+  def computer_shot_sinks(cell)
+    puts ""
+    puts "Aye, my shot on #{cell} sunk your ship!"
   end
 
   def player_wins
-    p "Shiver me timbers, you sunk all my ships!  You won!"
+    puts ""
+    puts "Shiver me timbers, you sunk all my ships!  You won!"
   end
 
   def computer_wins
-    p "I won!"
+    puts ""
+    puts "I won!"
   end
 
-  def play_again?
-    p "Would you like to play another game?"
-    p "Enter p to play: "
+  def play_again
+    puts ""
+    puts "Would you like to play another game?"
+    puts ""
   end
 
   def player_quits
-    p "Enter q to quit: "
+    puts ""
+    puts "Enter q to quit: "
+  end
+
+  def game_ends
+    puts ""
+    puts "Goodbye! Thank you for playing."
   end
 end
